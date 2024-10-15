ff418d6ab0d71a7b5d68e4bdad00d1d695453f3d
elmegharbel-3 <marwanelmegharbel@gmail.com>
2024-10-15 04:01:24 +0300

    added file input + tested in wsl

--- upload-arduino/arduino-api.py
+++ upload-arduino/arduino-api.py
@@ -8,11 +8,12 @@ def index():
 def upload_arduino():
     if flask.request.method == "POST":
         data = flask.request.form.to_dict()
-        file_loc = data["file"]
+        file = flask.request.files["file"]
+        file.save(f"upload-arduino\saved\{file.filename}")
         fbqn = "-b " + data["fbqn"]
         port = "-p " + data["port"]
-        print("arduino-cli upload" + " " + fbqn + " " + port + " " + file_loc)
-    return flask.render_template('index.html',file_loc=file_loc,port=port,fbqn=fbqn)
+        """ print("arduino-cli upload" + " " + fbqn + " " + port + " " + file_loc) """
+    return flask.render_template('index.html',file=file,port=port,fbqn=fbqn),flask.redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080,host="0.0.0.0",threaded=False)
