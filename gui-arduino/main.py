43c9c88ba588cf4b77967fb3b3a82afd72b0d3d9
elmegharbel-3 <marwanelmegharabel@gmail.com>
2024-10-26 00:43:28 +0300

    show the output of command

--- gui-arduino/main.py
+++ gui-arduino/main.py
@@ -1,5 +1,6 @@
 import flask 
 import os 
+import subprocess
 app = flask.Flask(__name__) 
 print("hello world")
 @app.route('/') 
@@ -9,13 +10,20 @@ def index():
 def upload_arduino():     
     if flask.request.method == "POST":         
         data = flask.request.form.to_dict()         
-        thefile = flask.request.files['file']   
-        thefile.save("main-sketch/main-sketch.ino")   
-        file_loc = f"~/gui-arduino/main-sketch"
-        fbqn = "-b " + data["fbqn"]         
-        port = "-p " + data["port"]         
-        os.system("arduino-cli compile" + " " + fbqn + " " + file_loc)         
-        os.system("arduino-cli upload" + " " + fbqn + " " + port + " " + file_loc)     
-        return flask.render_template('index.html',file_loc=file_loc,port=port,fbqn=fbqn)  
+        thefile = flask.request.files['file'] 
+        file_name = thefile.filename  
+        dir_name = file_name.rstrip(".ino")
+        print(file_name)
+        if os.path.exists(f"sketches/{dir_name}") == False:
+            os.mkdir(f"sketches/{dir_name}")
+            thefile.save(f"sketches/{dir_name}/{file_name}")   
+            file_loc = f"sketches/{dir_name}"
+            fbqn = data["fbqn"]         
+            port = data["port"]         
+            compile = subprocess.run(["arduino-cli","compile","-b",fbqn,"-p",port,file_loc],stdout= subprocess.PIPE,universal_newlines=True)
+            upload = subprocess.run(["arduino-cli","upload","-b",fbqn,"-p",port,file_loc],stdout= subprocess.PIPE,universal_newlines=True)   
+            return flask.render_template('index.html',file_loc=file_loc,port=port,fbqn=fbqn,compile_res=compile.stdout,upload_res=upload.stdout)  
+        else:
+            return flask.render_template('index.html',exist=True)
     if __name__ == '__main__':     
         app.run(debug=True, port=8080,host="0.0.0.0",threaded=False)
